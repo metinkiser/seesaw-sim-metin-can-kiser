dcbc2537174308cc780a60fcd04985ee20e68368
metinkiser <metincankiser1903@gmail.com>
2025-11-06 02:57:21 +0300

    feat: create global state

---
+++ src/state.js
@@ -0,0 +1,15 @@
+export const state = {
+  theta: 0,
+  omega: 0,
+  paused: false,
+  beam: { length: 2.0 },
+  ppm: 200,
+
+  
+  nextWeight: 5,
+  objects: [],        
+  leftTotal: 0,       
+  rightTotal: 0,      
+   ghost: { active: false, x: 0 },
+     events: [],  
+};
